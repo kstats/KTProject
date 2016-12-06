4d70deddd4b4c90e522367471b71456eb6fee8d9
kstats <katstasaski@gmail.com>
2016-12-05 16:43:42 -0800

    Revamped pruned hybrid model

--- kt_pruned.py
+++ kt_pruned.py
@@ -22,15 +22,13 @@ print "gets here"
 
 for item in tqdm(kdd):
     if item['Anon Student Id'] not in students:
-        students[item['Anon Student Id']] = 1
+        students[item['Anon Student Id']] = len(students)
     list_of_skills = item['KC(SubSkills)'].split('~~')
     for concept in list_of_skills:
         if concept not in skills:
-            skills[concept] = 1
+            skills[concept] = len(skills)
     #randomly chooses an item to be the concept
     item['KC(SubSkills)'] = random.choice(list_of_skills)
-kdd = kdd[:int(len(kdd) / 4)]
-print len(kdd)
 
 print "done with loop"
 #this is the number of unique students that are found in the dataset
@@ -42,10 +40,10 @@ num_skills = len(skills.keys())
 N = len(kdd)
 
 # probability that a student will learn a concept after answering a given question GIVEN the student
-prob_learn_s = np.random.rand(num_students)
+prob_learn_s = np.random.uniform(0.01, 0.99, num_students)
 
 # probability that a student will learn a concept after answering a given question GIVEN the concept
-prob_learn_c = np.random.rand(num_skills)
+prob_learn_c = np.random.uniform(0.01, 0.99, num_skills)
 
 q = np.random.rand(num_skills)
 
@@ -67,47 +65,64 @@ def forward_pass():
     count = 0
     for item in tqdm(kdd):
         y = item['Correct First Attempt']
-        concept = list(skills.keys()).index(item['KC(SubSkills)'])
-        student = list(students.keys()).index(item['Anon Student Id'])
+        concept = skills[item['KC(SubSkills)']]
+        student = students[item['Anon Student Id']]
+        running_betas[count] = beta_message[student][concept]
         alpha = up_message(y, concept)
         beta = beta_message[student][concept]
-        intermediate = np.multiply(alpha, beta)
-        intermediate = np.array([intermediate,]*2)
+        intermediate = (alpha * beta).reshape((-1, 1)).repeat(2, axis=1)
         edge_pot = phi(student, concept)
-        beta_message[student][concept] = np.sum(intermediate * edge_pot, axis = 1) / (np.sum(beta_message[student][concept]) + 1e-5)
-        running_betas[count] = beta_message[student][concept]
+        new_msg = np.sum(intermediate * edge_pot, axis=0)
+        beta_message[student][concept] = new_msg / np.sum(new_msg)
         count += 1
 
 delta_message = np.ones((num_students, num_skills, 2))
-E_stored = np.zeros((num_students, num_skills, 2))
+E_stored = np.ones((num_students, num_skills,)) * -1
+E_end = np.zeros((num_students, num_skills,))
+model_learning_rate = 1e-3
 def backward_pass():
+    global prob_learn_s, prob_learn_c
     count = N-1
     kdd.reverse()
+    grad_learn_s = np.zeros(prob_learn_s.shape)
+    grad_learn_c = np.zeros(prob_learn_c.shape)
     for item in tqdm(kdd):
         y = item['Correct First Attempt']
-        concept = list(skills.keys()).index(item['KC(SubSkills)'])
-        student = list(students.keys()).index(item['Anon Student Id'])
+        concept = skills[item['KC(SubSkills)']]
+        student = students[item['Anon Student Id']]
         alpha = up_message(y, concept)
         delta = delta_message[student][concept]
-        intermediate = np.multiply(alpha, delta)
-        intermediate = np.array([intermediate, ] * 2)
-        edge_pot = phi(student, concept)
-        delta_message[student][concept] = np.sum(intermediate * edge_pot, axis=1) / (np.sum(delta_message[student][concept]) + 1e-5)
+        intermediate = (alpha*delta).reshape((-1, 1)).repeat(2, axis=1)
+        edge_pot = phi(student, concept).transpose()
+        new_msg = np.sum(intermediate * edge_pot, axis=0)
+        delta_message[student][concept] = new_msg / np.sum(new_msg)
         #this does the E-step
-        E_zi = up_message(y, concept) * running_betas[count - 1] * delta_message[student][concept]
-        E_zi = (E_zi[0]) / (E_zi[0] + E_zi[1] + 1e-5)
-        if E_stored[student][concept][0] == 0:
-            E_stored[student][concept] = E_zi
-        m_step(E_zi, y, student, concept)
-        count -=1
+        e_zi = up_message(y, concept) * running_betas[count] * delta_message[student][concept]
+        e_zi = (e_zi[0] + 1e-5) / (np.sum(e_zi) + 1e-5)
+        if E_end[student, concept] == 0:
+            E_end[student, concept] = e_zi
+        prev_ezi = E_stored[student, concept]
+        E_stored[student, concept] = e_zi
+        m_step(e_zi, y, student, concept, prev_ezi, grad_learn_s, grad_learn_c)
+        count -= 1
+    prob_learn_s += model_learning_rate * grad_learn_s
+    prob_learn_s[prob_learn_s>1.0] = 1.0
+    prob_learn_c += model_learning_rate * grad_learn_c
+    prob_learn_c[prob_learn_c>1.0] = 1.0
 
-model_learning_rate = 1e-3
-def m_step(E_zi, y, student, concept):
-    q[concept] = q[concept] - (float(y) - np.exp(float(E_zi + q[concept])) / (1 + np.exp(float(E_zi + q[concept]))))
-    prob_learn_s[student] = prob_learn_s[student] - ((1 - E_zi) * prob_learn_c[concept]) / (E_zi + (1 - E_zi) * prob_learn_c[concept] * prob_learn_s[student])
-    prob_learn_c[concept] = prob_learn_c[concept] - ((1 - E_zi) * prob_learn_s[student]) / (E_zi + (1 - E_zi) * prob_learn_c[concept] * prob_learn_s[student])
-
-
+def m_step(e_zi, y, student, concept, prev_ezi, gs, gc):
+    q[concept] += model_learning_rate * (float(y) - np.exp(e_zi + q[concept]) / (1 + np.exp(e_zi + q[concept])))
+    if prev_ezi > 0:
+        lr = prob_learn_s[student] * prob_learn_c[concept]
+        l = e_zi * prev_ezi + (1-e_zi) * prev_ezi * lr + (1-e_zi) * (1-prev_ezi) * (1-lr)
+        if l <= 0:
+            print('zi:{}'.format(e_zi))
+            print('pre zi: {}'.format(prev_ezi))
+            print('lr: {}'.format(lr))
+            print('l: {}'.format(l))
+        dlr = ((1-e_zi) * prev_ezi - (1-e_zi) * (1-prev_ezi)) / np.log(l + 1e-5)
+        gs[student] += dlr * prob_learn_c[concept]
+        gc[concept] += dlr * prob_learn_s[student]
 
 #Here is where we use alpha beta and EM to estimate these actual parameters
 print "doing forward pass now..."
@@ -121,12 +136,17 @@ total = 0.
 #TODO add in final testing here once parameters are set, determine test set and test our values.
 kdd_test = dr.KDD("data/algebra_2008_2009_test.txt")
 f = open("algebra_2008_2009_submission.txt", 'w')
+f.write('Row\tCorrect First Attempt')
 for item in kdd_test:
-    if item['KC(SubSkills)'] is not None and item['KC(SubSkills)'] in skills and item['Anon Student Id'] in students:
-        student = list(students.keys()).index(item['Anon Student Id'])
-        concept = list(skills.keys()).index(item['KC(SubSkills)'])
-        f.write(str(E_stored[student][concept][0]))
-        f.write("\n")
+    list_of_skills = item['KC(SubSkills)'].split('~~')
+    concept = random.choice(list_of_skills)
+    student = item['Anon Student Id']
+    if concept in skills and student in students:
+        concept = skills[concept]
+        student = students[student]
+        pyi = 1.0 / (1.0 + np.exp(- E_end[student, concept] - q[concept]))
+        lr = prob_learn_s[student] * prob_learn_c[concept]
+        E_end[student, concept] += lr * (1 - E_end[student, concept])
+        f.write('\n{}\t{}'.format(item['Row'], pyi))
     else:
-        f.write(str(0.9))
-        f.write("\n")
+        f.write('\n{}\t{}'.format(item['Row'], 0.9))
